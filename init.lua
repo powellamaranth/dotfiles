14b27edd2f5570fb3139d77ec43659a08f2a1cde
Daniel Gatis <danielgatis@gmail.com>
2022-04-06 23:49:04 -0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,9 +1,8 @@
-local packer_bootstrap = (function()
-    local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-    if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-        return vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    end
-end)()
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
 
 require'packer'.startup({function(use)
     use { 'wbthomason/packer.nvim' }
