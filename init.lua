ef23e8fd7fd151cebec5c87e8fcbf84e19cf7b5e
Daniel Gatis <danielgatis@gmail.com>
2022-04-21 19:29:12 -0300

    vimrc

--- init.lua
+++ init.lua
@@ -6,16 +6,6 @@ end
 
 require'packer'.startup(function(use)
   use { 'wbthomason/packer.nvim' }
-  use { 'folke/which-key.nvim' }
-  use { 'lewis6991/impatient.nvim' }
-  use { 'nvim-lua/plenary.nvim' }
-  use { 'rcarriga/nvim-notify' }
-  use { 'antoinemadec/FixCursorHold.nvim' }
-  use { 'kyazdani42/nvim-web-devicons' }
-  use { 'akinsho/bufferline.nvim' }
-  use { 'nvim-lualine/lualine.nvim' }
-  use { 'norcalli/nvim-colorizer.lua' }
-  use { 'max397574/better-escape.nvim' }
   use { 'dracula/vim' }
 
   if packer_bootstrap then
