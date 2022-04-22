2d4920fd29cb80b2e6864a5bf609e677a21ba61e
Daniel Carrazzoni <danielcarrazzoni@BRSPPM-C02GL2HMMD6M.local>
2022-04-22 17:01:23 -0300

    add home folder

---
+++ home/.config/hammerspoon/init.lua
@@ -0,0 +1,28 @@
+-- Hot reload config
+hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', hs.reload):start()
+hs.alert('🔨🥄')
+
+-- Spaces hotkeys
+function set_spaces_hotkeys()
+  local main_screen_uuid = hs.screen.mainScreen():getUUID()
+  local spaces = hs.spaces.allSpaces()
+
+  for i = 1,9,1 do
+    local space_id = spaces[main_screen_uuid][i]
+
+    hs.hotkey.deleteAll({"cmd"}, tostring(i))
+    hs.hotkey.bind({"cmd"}, tostring(i), function()
+      if space_id then
+        hs.spaces.gotoSpace(space_id)
+        return
+      end
+
+      hs.spaces.addSpaceToScreen(main_screen_uuid)
+      local spaces = hs.spaces.allSpaces()[main_screen_uuid]
+      hs.spaces.gotoSpace(spaces[#spaces])
+    end)
+  end
+end
+
+set_spaces_hotkeys()
+hs.spaces.watcher.new(set_spaces_hotkeys):start()
