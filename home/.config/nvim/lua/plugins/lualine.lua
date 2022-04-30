642dbfbc86f7739c0a37111767ac2940d9c08a5a
Daniel Gatis <danielgatis@gmail.com>
2022-04-30 15:21:13 -0300

    update nvim

--- home/.config/nvim/lua/plugins/lualine.lua
+++ home/.config/nvim/lua/plugins/lualine.lua
@@ -6,20 +6,20 @@ local function config()
       globalstatus = true,
     },
     sections = {
-      lualine_a = {'mode'},
-      lualine_b = {'branch', 'diff', 'diagnostics'},
-      lualine_c = {'filename'},
+      lualine_a = {"mode"},
+      lualine_b = {"branch", "diff", "diagnostics"},
+      lualine_c = {"filename"},
       lualine_x = {},
-      lualine_y = {'encoding', 'fileformat', 'filetype'},
-      lualine_z = {'location'}
+      lualine_y = {"encoding", "fileformat", "filetype"},
+      lualine_z = {"location"}
     },
     inactive_sections = {
       lualine_a = {},
       lualine_b = {},
-      lualine_c = {'filename'},
+      lualine_c = {"filename"},
       lualine_x = {},
       lualine_y = {},
-      lualine_z = {'location'},
+      lualine_z = {"location"},
     },
     extensions = {"quickfix", "nvim-tree"},
   })
