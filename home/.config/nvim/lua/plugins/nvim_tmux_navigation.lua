650187957ef597e96bbb73179471154bacd6b128
Daniel Gatis <danielgatis@gmail.com>
2022-07-05 20:17:38 -0300

    nvim

--- home/.config/nvim/lua/plugins/nvim_tmux_navigation.lua
+++ home/.config/nvim/lua/plugins/nvim_tmux_navigation.lua
@@ -4,10 +4,11 @@ local function config()
   local keymap = vim.keymap.set
   local opts = { noremap = true, silent = true }
 
-  keymap("n", "<m-h>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLeft()<cr>", opts)
-  keymap("n", "<m-j>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateDown()<cr>", opts)
-  keymap("n", "<m-k>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateUp()<cr>", opts)
-  keymap("n", "<m-l>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateRight()<cr>", opts)
+  keymap("n", "<m-left>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLeft()<cr>", opts)
+  keymap("n", "<m-down>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateDown()<cr>", opts)
+  keymap("n", "<m-up>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateUp()<cr>", opts)
+  keymap("n", "<m-right>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateRight()<cr>", opts)
+
   keymap("n", "<m-/>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLastActive()<cr>", opts)
   keymap("n", "<m-space>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateNext()<cr>", opts)
 end
